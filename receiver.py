ba4a57e36a9449ffabf63c4f60fd20f388476a3f
alaidine <aa.alimlinde@gmail.com>
2025-11-28 14:55:41 +0400

    Use argparse

--- receiver.py
+++ receiver.py
@@ -1,7 +1,16 @@
 from vidstream import StreamingServer
 import threading
+import argparse
 
-receiver = StreamingServer("127.0.0.1", 9999)
+parser = argparse.ArgumentParser(
+    prog='receiver.py',
+    description='receiver part of Chrome Cast Home Made'
+)
+
+parser.add_argument('-p', '--port', type=int)
+args = parser.parse_args()
+
+receiver = StreamingServer("127.0.0.1", args.port)
 t = threading.Thread(target=receiver.start_server)
 t.start()
 
--- sender.py
+++ sender.py
@@ -1,4 +1,17 @@
 from vidstream import ScreenShareClient
+import argparse
 
-sender = ScreenShareClient("127.0.0.1", 9999)
+parser = argparse.ArgumentParser(
+    prog='sender.py',
+    description='sender part of Chrome Cast Home Made'
+)
+
+parser.add_argument('-p', '--port', type=int)
+parser.add_argument('-ip', '--host', type=str)
+parser.add_argument('-x', '--width', type=int)
+parser.add_argument('-y', '--height', type=int)
+
+args = parser.parse_args()
+
+sender = ScreenShareClient(args.host, args.port, args.width, args.height)
 sender.start_stream()
