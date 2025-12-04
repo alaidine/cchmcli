22fae4817971ea75da45c3aac9f82ba55daa0232
alaidine <aa.alimlinde@gmail.com>
2025-12-04 11:01:13 +0400

    Update

--- receiver.py
+++ receiver.py
@@ -1,6 +1,16 @@
 from vidstream import StreamingServer
 import threading
 import argparse
+import socket
+
+def get_local_ip():
+    """Get the local IP address that can be used by other machines"""
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))
+            return s.getsockname()[0]
+    except Exception:
+        return "127.0.0.1"
 
 parser = argparse.ArgumentParser(
     prog='receiver.py',
@@ -10,7 +20,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-p', '--port', type=int)
 args = parser.parse_args()
 
-receiver = StreamingServer("127.0.0.1", args.port)
+# Get local IP to accept connections from other computers
+local_ip = get_local_ip()
+print(f"Starting receiver on {local_ip}:{args.port}")
+print(f"Connect from other computers using: {local_ip}:{args.port}")
+
+receiver = StreamingServer(local_ip, args.port)
 t = threading.Thread(target=receiver.start_server)
 t.start()
 
